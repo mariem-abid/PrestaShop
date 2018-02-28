8891588b5b71c38fffdf624f054d98c94c0d0526
mariem abid <mariem.abid@sifast.com>
2018-02-28 17:17:23 +0100

    add carrier

---
+++ tests/E2E/test/clients/carrier.js
@@ -0,0 +1,8 @@
+var CommonClient = require('./common_client');
+class Carrier extends CommonClient {
+    
+    
+    
+}
+
+module.exports = Carrier;
---
+++ tests/E2E/test/selectors/BO/catalogpage/carrier_submenu.js
@@ -0,0 +1,18 @@
+module.exports = {
+    CarrierSubMenu: {
+
+        submenu: '//*[@id="subtab-AdminCarriers"]/a',
+        new_carrier_button: '//*[@id="page-header-desc-carrier-new_carrier"]/i',
+        name_input: '//*[@id="name"]',
+        transit_time: '//*[@id="delay_1"]',
+        speed_grade: '//*[@id="grade"]',
+        logo: '//*[@id="carrier_logo_input"]',
+        tracking_url: '//*[@id="url"]',
+        
+
+
+    }
+
+
+};
+
