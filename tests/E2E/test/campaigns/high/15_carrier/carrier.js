bed996c8055401e085277acc56f8d0edeea58bdf
mariem abid <mariem.abid@sifast.com>
2018-03-08 17:01:51 +0100

    CRUD Carrier

--- tests/E2E/test/campaigns/high/15_carrier/carrier.js
+++ tests/E2E/test/campaigns/high/15_carrier/carrier.js
@@ -19,20 +19,22 @@ scenario('Create "CRUD Carrier"', () => {
       test('should set the "Name" input', () => client.waitAndSetValue(CarrierSubMenu.name_input, 'carrier' + date_time));
       test('should set the "Transit time" input', () => client.waitAndSetValue(CarrierSubMenu.transit_time, '5 heures'));
       test('should upload the logo', () => client.uploadPicture('carrier_image.jpeg', CarrierSubMenu.logo, 'image'));
-      test('should click on "Next" button', () => client.waitForExistAndClick(CarrierSubMenu.next_button));
+      test('should click on the "Next" button', () => client.waitForExistAndClick(CarrierSubMenu.next_button));
       test('should enable the "Add handling costs" ', () => client.waitForExistAndClick(CarrierSubMenu.shipping_handling.replace('%id', 1), 1000));
-      test('should click on "According to total price"', () => client.waitForExistAndClick(CarrierSubMenu.billing_price));
+      test('should choose the "According to total price"', () => client.waitForExistAndClick(CarrierSubMenu.billing_price));
       test('should set the "price inf " input', () => client.waitAndSetValue(CarrierSubMenu.range.replace('%id', 1), 0));
       test('should set the "price sup " input', () => client.waitAndSetValue(CarrierSubMenu.range.replace('%id', 2), 10000));
       test('should check all zones', () => client.waitForExistAndClick(CarrierSubMenu.all_zone, 1000));
       test('should set the "price per zone " input', () => client.waitAndSetValue(CarrierSubMenu.range.replace('%id', 3), 12));
-      test('should click on "Next" button', () =>client.scrollWaitForExistAndClick(CarrierSubMenu.next_button, 60));
+      test('should click on the "Next" button', () =>client.scrollWaitForExistAndClick(CarrierSubMenu.next_button, 60));
       test('should set the "Maximum package width (cm) " input', () => client.waitAndSetValue(CarrierSubMenu.max_width, 100));
       test('should set the "Maximum package height (cm) " input', () => client.waitAndSetValue(CarrierSubMenu.max_hieght, 100));
       test('should set the "Maximum package depth (cm)" input', () => client.waitAndSetValue(CarrierSubMenu.max_depth, 100));
       test('should set the "Maximum package weight (kg)" input', () => client.waitAndSetValue(CarrierSubMenu.max_weight, 100));
       test('should click on "Next" button', () => client.waitForExistAndClick(CarrierSubMenu.next_button));
       test('should click on "finish" button', () => client.waitForExistAndClick(CarrierSubMenu.finish_button));
+      test('should verify the appearance of the green validation', () => client.checkTextValue(CarrierSubMenu.green_validation, '×\nSuccessful creation.'));
+
     }, 'carrier');
   }, 'carrier');
 
@@ -40,29 +42,31 @@ scenario('Create "CRUD Carrier"', () => {
     test('should search for carrier ', () => client.searchByValue(CarrierSubMenu.search_input, CarrierSubMenu.search_button, 'carrier' + date_time));
     test('should click on "Edit" action', () => client.waitForExistAndClick(CarrierSubMenu.update_button));
     test('should click on "Next" button', () => client.waitForExistAndClick(CarrierSubMenu.next_button));
-    test('should click on "According to total weight"', () => client.waitForExistAndClick(CarrierSubMenu.billing_weight, 1000));
+    test('should choose the "According to total weight"', () => client.waitForExistAndClick(CarrierSubMenu.billing_weight, 1000));
     test('should set the "weight inf " input', () => client.waitAndSetValue(CarrierSubMenu.range.replace('%id', 1), 0.1));
     test('should set the "weight sup " input', () => {
       return promise
         .then(() =>client.waitAndSetValue(CarrierSubMenu.range.replace('%id', 2), 10))
         .then(() =>client.pause(1000))
     });
-    test('should set the "price per zone " input', () => client.waitAndSetValue(CarrierSubMenu.range.replace('%id', 3), 8));
-    test('should click on "finish" button', () =>client.scrollWaitForExistAndClick(CarrierSubMenu.finish_button, 60));
+    test('should set the "price per zone" input', () => client.waitAndSetValue(CarrierSubMenu.range.replace('%id', 3), 8));
+    test('should click on the "finish" button', () =>client.scrollWaitForExistAndClick(CarrierSubMenu.finish_button, 60));
     test('should verify the appearance of the green validation', () => client.checkTextValue(CarrierSubMenu.green_validation, '×\nSuccessful update.'));
   }, 'carrier');
 
   scenario('Delete carrier', client => {
     test('should click on the icon', () => client.waitForExistAndClick(CarrierSubMenu.icon, 1000));
-    test('should click on "Delete" button', () => {
+    test('should click on the "Delete" button', () => {
       return promise
         .then(() =>client.waitForExistAndClick(CarrierSubMenu.delete_button))
         .then(() =>client.alertAccept())
     });
-  }, 'carrier')
+    test('should verify the appearance of the green validation', () => client.checkTextValue(CarrierSubMenu.green_validation, '×\nSuccessful deletion.'));
+
+  }, 'carrier');
 
   scenario('Logout from the Back Office', client => {
     test('should logout successfully from Back Office', () => client.signOutBO());
   }, 'carrier');
-}, 'category', true);
+}, 'carrier');
 
