09f99b12787603ab2d66d0e783de47c382eaa3c4
mariem abid <mariem.abid@sifast.com>
2018-04-23 16:51:21 +0100

    Onboarding

--- tests/E2E/test/selectors/BO/onboarding.js
+++ tests/E2E/test/selectors/BO/onboarding.js
@@ -5,7 +5,7 @@ module.exports = {
     popup_close_button: '/html/body/div[1]/div/div/div[3]/button[1]',
     stop_button: '.onboarding-button-stop',
     start_button: '//div[@class="onboarding-welcome"]//button[contains(text(), "Start")]',
-    resume_button: '/html/body/nav/div/div[3]/button', /*'//*[@id="nav-sidebar"]//button[contains(@class, "resume")]'*/
+    resume_button: '//*[@id="nav-sidebar"]//button[contains(@class, "resume")]',
     ready_button: '//*[@id="onboarding-welcome"]//button[contains(@class, "onboarding-button-next") and contains(text(), "I\'m ready")]',
     payement_check_button: '//*[@id="content"]//a[contains(@href,"%s")]',
     welcomeSteps: {
@@ -15,7 +15,11 @@ module.exports = {
       tutorial_step: '//div[@class="onboarding-advancement"]//div[@class="advancement-groups"]//div[@class="group group-%P"]//div[@class="id -done"]',
       tooltip_step: '//div[contains(@class, "onboarding-tooltip")]//span',
       understand_button: '/html/body/div/div[2]/a[1]',
-      header_logo: '//*[@id="PS_LOGO-selectbutton"]'
+      header_logo: '//*[@id="PS_LOGO-selectbutton"]',
+      starter_guide_button: '//*[@id="onboarding-welcome"]//div[@class="starter-guide"]',
+      forum_button: '//*[@id="onboarding-welcome"]//div[@class="forum"]',
+      training_button: '//*[@id="onboarding-welcome"]//div[@class="training"]',
+      video_tutorial_button: '//*[@id="onboarding-welcome"]//div[@class="video-tutorial"]'
     }
   }
 };
