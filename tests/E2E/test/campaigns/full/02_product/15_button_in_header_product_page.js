9ea57f5fe1b1eb73ab76737f07fff4b4bfc124d1
mariem-abid <marieem.abid@gmail.com>
2018-05-29 09:00:23 +0100

    buttons in header product page

--- tests/E2E/test/campaigns/full/02_product/15_button_in_header_product_page.js
+++ tests/E2E/test/campaigns/full/02_product/15_button_in_header_product_page.js
@@ -1,13 +1,14 @@
 const {AccessPageBO} = require('../../../selectors/BO/access_page');
 const {Menu} = require('../../../selectors/BO/menu.js');
 const {AddProductPage} = require('../../../selectors/BO/add_product_page');
+let promise = Promise.resolve();
 
 scenario('Check if the buttons in the header of the product page ', client => {
   test('should open browser', () => client.open());
   test('should login successfully in the Back Office', () => client.signInBO(AccessPageBO));
   test('should go to "Catalog" page', () => client.goToSubtabMenuPage(Menu.Sell.Catalog.catalog_menu, Menu.Sell.Catalog.products_submenu));
   test('should click on "NEW PRODUCT"', () => client.waitForExistAndClick(AddProductPage.new_product_button));
- // test('should check the product name is empty by default and the text "Enter the name of your product" appears', () => client.checkAttributeValue(AddProductPage.product_name_input,'placeholder','Enter your product name','equal'));
+  test('should check the product name is empty by default and the text "Enter the name of your product" appears', () => client.checkAttributeValue(AddProductPage.product_name_input,'placeholder','Enter your product name','equal'));
   test('should set the "product name"', () => client.waitAndSetValue(AddProductPage.product_name_input, 'product'+ date_time));
   test('should close symfony Profiler', () => {
     return promise
