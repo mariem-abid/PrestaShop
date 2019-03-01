780b5661ae0ad33c48d9db144bbf01d9c682456c
Mariem ABID <marieem.abid@gmail.com>
2019-03-01 15:27:39 +0100

    TE:index

--- tests/E2Epuppeteer/test/selectors/BO/catalogpage/index.js
+++ tests/E2Epuppeteer/test/selectors/BO/catalogpage/index.js
@@ -1,25 +1,25 @@
 module.exports = Object.assign(
   {
     CatalogPage: {
-      success_panel: '//*[@id="content"]/div[@class="bootstrap"]/div[contains(@class, "success")]',
-      danger_panel: '//*[@id="content"]/div[@class="bootstrap"]/div[contains(@class, "danger")]',
-      select_all_product_button: '//*[@id="bulk_action_select_all"]/..',
-      action_group_button: '//*[@id="product_bulk_menu"]',
-      action_button: '(//*[@id="main-div"]//div[contains(@class, "bulk-catalog")]//a)[%ID]',
-      green_validation: '//*[@id="main-div"]//div[contains(@class, "alert-success") and not(@style)]',
-      product_status_icon: '(//*[@id="product_catalog_list"]//tbody/tr[%S]//i[contains(@class, "material-icons")])[1]',
-      name_search_input: '(//*[@id="product_catalog_list"]//input[contains(@name, "filter_column_name")])[1]',
-      search_button: '//*[@id="product_catalog_list"]//button[contains(@name, "products_filter_submit")]',
-      dropdown_toggle: '//*[@id="product_catalog_list"]//button[contains(@class, "dropdown-toggle")]',
-      delete_button: '//*[@id="product_catalog_list"]//a[contains(@onclick, "delete")]',
-      delete_confirmation: '//*[@id="catalog_deletion_modal"]//button[2]',
-      close_delete_modal: '(//*[@id="catalog_deletion_modal"]//button[1])[2]',
-      reset_button: '//*[@id="product_catalog_list"]//button[contains(@name, "products_filter_reset")]',
-      search_result_message: '//*[@id="product_catalog_list"]//td[contains(text(), "There is no result for this search")]',
-      deactivate_modal: '//*[@id="catalog_deactivate_all_modal"]',
-      activate_modal: '//*[@id="catalog_activate_all_progression"]//div[contains(text(), "Activating")]',
-      duplicate_modal: '//*[@id="catalog_duplicate_all_modal"]',
-      delete_modal: '//*[@id="catalog_deletion_modal"]'
+      success_panel: '#content > div.bootstrap > div[class*=success]',
+      danger_panel: '#content > div.bootstrap > div[class*=danger]',
+      select_all_product_button: '#catalog-actions > div:nth-child(2) > div > div > label',
+      action_group_button: '#product_bulk_menu',
+      action_button: '#catalog-actions a[onclick*=bulkProductAction]:nth-child(%ID)',
+      green_validation: '#main-div  div[class="alert alert-success"][role=alert]',
+      product_status_icon: '#product_catalog_list  tr:nth-child(%S) td:nth-child(10) i[class*=material-icons]',
+      name_search_input: '#product_catalog_list  input[name=filter_column_name]',
+      search_button: '#product_catalog_list button[name*=products_filter_submit]',
+      dropdown_toggle: '#product_catalog_list button[class*=dropdown-toggle]',
+      delete_button: '#product_catalog_list a[onclick*=delete]',
+      delete_confirmation: '#catalog_deletion_modal button:nth-of-type(2)',
+      close_delete_modal: '#catalog_deletion_modal  button.btn.btn-outline-secondary',
+      reset_button: '#product_catalog_list button[name*=products_filter_reset]',
+      search_result_message: '//*[@id="product_catalog_list"]//td[contains(text(), "There is no result for this search")]',//a changer
+      deactivate_modal: '#catalog_deactivate_all_modal',
+      activate_modal: '//*[@id="catalog_activate_all_progression"]//div[contains(text(), "Activating")]',//a changer
+      duplicate_modal: '#catalog_duplicate_all_modal',
+      delete_modal: '#catalog_deletion_modal'
     }
   },
   require('./feature_submenu'),
